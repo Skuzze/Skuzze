7f2e6df20ebb1c270f61b4683652924cdfead343
Skuzze <104659087+Skuzze@users.noreply.github.com>
2022-04-30 09:24:34 -0300

    Create HANGMANGAME.py

---
+++ HANGMANGAME.py
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @Skuzze
+- 👀 I’m interested in cyber security and knowledge Exchange
+- 🌱 I’m currently learning Software Engineering
+- 💞️ I’m looking to collaborate on JS and python files
+- 📫 How to reach me lgferreira007@gmail.com
+
+<!---
+Skuzze/Skuzze is a ✨ special ✨ repository because its `HANGMANGAME.py` () appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+---> 
